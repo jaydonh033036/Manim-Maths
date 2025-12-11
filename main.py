06a86c7faf402743c714b49b3249f4756f068faf
jaydonh033036 <Jaydon.hodson@gmail.com>
2025-12-11 22:24:08 +0000

    Importing Manim and Test function to write HelloWorld

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print('hello world!')
+from manim import *
+
+
+class HelloWorld(Scene):
+    def construct(self):
+        # 1. Create the text Mobject (Manim Object)
+        hello = Text("Hello, World!")
+
+        # 2. Display the text on the screen
+        self.play(Write(hello))
+
+        # 3. Wait for 1 second before the animation ends
+        self.wait(1)
